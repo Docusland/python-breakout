65503de32c1fc17184a289c9e24840e9e3857d6c
Docusland <erwann.duclos@gmail.com>
2022-02-01 10:45:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Contexte
+
+Ce projet a été réalisé en suivant un tutoriel présent ici : 
+ - https://www.101computing.net/breakout-tutorial-using-pygame-getting-started/
+
+# Objectif premier
+Faites en sorte que ce projet compile et puisse être executé à partir de
+ votre IDE.
+
+# Exercices
+## Exercice 1
+Vous avez uniquement 3 vies et ce jeu est assez difficile. 
+Faites en sorte d'avoir 5 vies.
+
+## Exercice 2
+
+En jouant un peu, on peut se rendre compte qu'il y a quelques problèmes au
+ sein de l'application. 
+ 
+ En effet, parfois la balle rebondis étrangement et reste horizontale.
+ Trouvez commment corriger cette anomalie. 
+
+## Exercice 3
+Un message est affiché lors
---
+++ models.py
@@ -0,0 +1,87 @@
+import pygame
+from random import randint
+from colors import *
+
+
+class Paddle(pygame.sprite.Sprite):
+    # This class represents a paddle. It derives from the "Sprite" class in Pygame.
+
+    def __init__(self, color, width, height):
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+
+        # Pass in the color of the paddle, its width and height.
+        # Set the background color and set it to be transparent
+        self.image = pygame.Surface([width, height])
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+
+        # Draw the paddle (a rectangle!)
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+
+        # Fetch the rectangle object that has the dimensions of the image.
+        self.rect = self.image.get_rect()
+
+    def moveLeft(self, pixels):
+        self.rect.x -= pixels
+        # Check that you are not going too far (off the screen)
+        if self.rect.x < 0:
+            self.rect.x = 0
+
+    def moveRight(self, pixels):
+        self.rect.x += pixels
+        # Check that you are not going too far (off the screen)
+        if self.rect.x > 700:
+            self.rect.x = 700
+
+
+class Ball(pygame.sprite.Sprite):
+    # This class represents a car. It derives from the "Sprite" class in Pygame.
+
+    def __init__(self, color, width, height):
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+
+        # Pass in the color of the car, and its x and y position, width and height.
+        # Set the background color and set it to be transparent
+        self.image = pygame.Surface([width, height])
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+
+        # Draw the ball (a rectangle!)
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+
+        self.velocity = [randint(4, 8), randint(-8, 8)]
+
+        # Fetch the rectangle object that has the dimensions of the image.
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+
+    def bounce(self):
+        # Mirror effect on x
+        self.velocity[0] = -self.velocity[0]
+
+        # Randomize angle. But should not be null
+        self.velocity[1] = randint(-8, 8)
+
+class Brick(pygame.sprite.Sprite):
+    # This class represents a brick. It derives from the "Sprite" class in Pygame.
+
+    def __init__(self, color, width, height):
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+
+        # Pass in the color of the brick, and its x and y position, width and height.
+        # Set the background color and set it to be transparent
+        self.image = pygame.Surface([width, height])
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+
+        # Draw the brick (a rectangle!)
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+
+        # Fetch the rectangle object that has the dimensions of the image.
+        self.rect = self.image.get_rect()
